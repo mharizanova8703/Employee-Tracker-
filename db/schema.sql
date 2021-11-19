a27bd048a1cdf8027e29d5d34362343ff93db4eb
mharizanova8703 <85656320+mharizanova8703@users.noreply.github.com>
2021-11-18 21:33:59 -0500

    creating db folder
    creating schema.sql and seeds file where i will create a data base tables

---
+++ db/schema.sql
@@ -0,0 +1,44 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE departments (
+
+id INT NOT NULL AUTO_INCREMENT,
+
+deptname VARCHAR(30) NOT NULL,
+
+PRIMARY KEY(id)
+
+);
+
+CREATE TABLE roles (
+
+id INT NOT NULL AUTO_INCREMENT,
+
+title VARCHAR(30) NOT NULL,
+
+salary DECIMAL(10,2) NOT NULL,
+
+department_id INT NOT NULL,
+
+PRIMARY KEY (id)
+
+);
+
+CREATE TABLE employees (
+
+id INT NOT NULL AUTO_INCREMENT,
+
+first_name VARCHAR(30) NOT NULL,
+
+last_name VARCHAR(30) NOT NULL,
+
+role_id INT NOT NULL,
+
+manager_id INT,
+
+PRIMARY KEY (id)
+
+);
